86f69a73182fe118d26d8bef9969bf0c1b8ee9ff
1m4d99 <imadnouioura99@gmail.com>
2024-10-02 12:18:41 +0200

    new

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Test-in-node
+some test in nodeJS
---
+++ test.js
@@ -0,0 +1,30 @@
+                        // type of
+const firstName = "Bob"// string
+const age = 34.5 // number
+const admin = false // boolean
+
+
+const userOne = {
+    firstName : "Jhon",
+    lastName : "Conor",
+    age : 16,
+    admin : true 
+}
+
+// Modifier la valeur age
+ userOne.age = 23
+
+const userTwo = {
+    firstName : "Sarah",
+    lastName : "Conor",
+    age : 52,
+    admin : false 
+}
+
+const users = [userOne,userTwo, "hello test"]
+
+console.log(users[0])
+
+// array 
+
+const.users = 
